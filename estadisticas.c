7761d721021c37acd6a7e2a4627e0ca8a691f34e
Kigo524 <kigocode@gmail.com>
2025-05-08 19:18:45 -0600

    Implementa la estructura básica de la calculadora de estadísticas para calcular media, mediana y moda.

---
+++ estadisticas.c
@@ -0,0 +1,13 @@
+/*Calculadora de Estadísticas:
+Crea un programa que permita al usuario ingresar una serie de números 
+y luego calcule la media, mediana y moda de esos números. 
+Utiliza arrays para almacenar los datos.*/
+//voy a usar 10 numeros solo para ver que funciona.
+
+#include<stdio.h>
+int main(){
+    int nums[10];
+    int cont = 0, suma = 0, moda = 0;
+
+    
+}
